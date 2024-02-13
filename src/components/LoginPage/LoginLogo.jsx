91fc1750267395f4c015d22ccbed8dc76f04548c
bumseok <bs950929@naver.com>
2024-02-13 17:40:30 +0900

    범석 로그인화면 커밋

--- src/components/LoginPage/LoginLogo.jsx
+++ src/components/LoginPage/LoginLogo.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const LoginLogo = () => {
   return (
+    <div className='loginLogoContainer'>
     <div style={{ height : "224px"}}>
       <img src="https://img.freepik.com/premium-vector/bamboo-logo-template-vector-icon-illustration-design_9999-19885.jpg" 
       style={{height:'100%'}} />
     </div>
+     <div style={{height:"50px"}}>
+     <img src="https://befe.co.kr/data/bbsData/16454404861.png"
+     style={{height:'100%'}} />
+ </div>
+ </div>
   )
 }
 
