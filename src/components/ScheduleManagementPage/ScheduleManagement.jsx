32e77d5dd7735dc8e40dc815a096a5868691c4c3
bumseok <bs950929@naver.com>
2024-02-14 10:37:46 +0900

    범석 일정 커밋

--- src/components/ScheduleManagementPage/ScheduleManagement.jsx
+++ src/components/ScheduleManagementPage/ScheduleManagement.jsx
@@ -8,13 +8,14 @@ import { useState } from 'react'
 const ScheduleManagement = () => {
 
   const [scheduleInput, setScheduleInput] = useState([
-    { title: '프로젝트 시작', date: '2024-02-01', key: 1},
-    { title: '수건수량 분석', date: '2024-02-02', key: 2}
+    { title: '프로젝트 시작', date: '2024-02-01', key: 1, color:"green"},
+    { title: '수건수량 분석', date: '2024-02-02', key: 2, color:"black"}
   ]);
 
   const [scheduleDate, setScheduleDate] = useState();
   const [quantity, setQuantity] = useState();
-  const [store, setStore] = useState();
+  const [store, setStore] = useState('입고');
+  const [scheduleColor, setScheduleColor] = useState("blue");
 
 
 
@@ -28,11 +29,12 @@ const ScheduleManagement = () => {
     if(scheduleInput.length>0){
       scheduleKey = scheduleInput[scheduleInput.length -1].key +1
     }
-
+   
     setScheduleInput([
       ...scheduleInput,
-      {title : quantity+store, date: scheduleDate, key:scheduleKey}
+      {title : quantity+store, date: scheduleDate, key:scheduleKey, color:scheduleColor}
     ])
+
 
   }
 
@@ -46,7 +48,8 @@ const ScheduleManagement = () => {
       scheduleInput, setScheduleInput,
       scheduleDate, setScheduleDate,
       quantity, setQuantity,
-      store, setStore
+      store, setStore,
+      scheduleColor, setScheduleColor
     }}>
 
     <div>
