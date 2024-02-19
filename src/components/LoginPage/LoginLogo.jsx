0ca1743bb6a6353b8cd5a8bed138e5f8e1fb81ec
Gim <rdonggyun0811@gmail.com>
2024-02-19 09:03:37 +0900

    ehdrbs pull

--- src/components/LoginPage/LoginLogo.jsx
+++ src/components/LoginPage/LoginLogo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import bamboo from '../../image/bamboo.gif'
 const LoginLogo = () => {
   return (
     <div className='loginLogoContainer'>
     <div style={{ height : "224px"}}>
-      <img src="https://img.freepik.com/premium-vector/bamboo-logo-template-vector-icon-illustration-design_9999-19885.jpg" 
-      style={{height:'100%'}} />
+      <img src={bamboo} alt="" style={{marginRight:"70px"}}/>
+      {/* <img src="https://img.freepik.com/premium-vector/bamboo-logo-template-vector-icon-illustration-design_9999-19885.jpg"  */}
+      {/* style={{height:'100%'}} /> */}
     </div>
      <div style={{height:"50px"}}>
      <img src="https://befe.co.kr/data/bbsData/16454404861.png"
