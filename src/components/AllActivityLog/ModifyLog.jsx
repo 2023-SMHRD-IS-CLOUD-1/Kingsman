77bbb4df094c0ebd1434da3585a66742e26656ff
Gim <rdonggyun0811@gmail.com>
2024-02-20 15:47:54 +0900

    ehdrbs pull

--- src/components/AllActivityLog/ModifyLog.jsx
+++ src/components/AllActivityLog/ModifyLog.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom';
 
 const ModifyLog = () => {
+
+  const nav = useNavigate();
+
   return (
     <div className='modifyLog'>
-        <button className='modifyButton'>수정</button>
+        <button className='modifyButton' onClick={() => { nav('/usercounttowel') }}>수정</button>
     </div>
   )
 }
