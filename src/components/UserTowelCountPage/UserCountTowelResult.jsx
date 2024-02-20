c48c0fd35db64cead82de320b87ac7895cf2a7d9
박재욱 <jaeuk0505@gmail.com>
2024-02-20 14:27:01 +0900

    jaeuk

--- src/components/UserTowelCountPage/UserCountTowelResult.jsx
+++ src/components/UserTowelCountPage/UserCountTowelResult.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import analyzing from '../../image/analyzing.png'
 import { CircularProgress } from '@mui/material'
 import { useContext } from 'react'
 import { UserCountTowelContext } from '../../context/UserCountTowelContext'
+import axios from 'axios'
 
 const UserCountTowelResult = () => {
 
   const { results, setResults } = useContext(UserCountTowelContext);
+  const id  = sessionStorage.getItem("user")
+  const sendPromotionalData = () => {
+   
+    let t_COUNT = results === "50개 입니다." ? 50 : 0;
+        const payload2 = {
+          
+            t_COUNT :t_COUNT ,
+            t_RESULT : results,
+            t_ID : id
+        };
+        console.log('payload2 값 확인:', payload2);
+        axios
+            .post('http://localhost:8085/kingsman/CountTowel', payload2, { withCredentials: true })
+            .then((response) => {
+                console.log('데이터 전송 성공:', response.data);
+            })
+            .catch((error) => {
+                console.error('데이터 전송 중 오류:', error);
+            });
+    
+};
 
   return (
     <div className='UserCountTowelResult'>
