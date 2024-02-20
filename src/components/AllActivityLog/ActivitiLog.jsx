78a5f9d66556cb7f7595a5079388a272f8d94722
Gim <rdonggyun0811@gmail.com>
2024-02-20 14:28:27 +0900

    ehdrbs pull

--- src/components/AllActivityLog/ActivitiLog.jsx
+++ src/components/AllActivityLog/ActivitiLog.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import ActivitiLogButtons from './ActivitiLogButtons';
 import towelSample from '../../image/sampleTowel.jpg';
 import axios from 'axios';
-import AWS from 'aws-skd';
 
 const ActivitiLog = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -98,6 +97,8 @@ const ActivitiLog = () => {
   //   };
   //   fetchData3();
   // }, []);
+
+  
 
   return (
     <div className='activitiLog'>
