f28742aed35f29ee7ef989e373aaab4cb0acb06b
hanmyeonghun <myeonghunhan732@gmail.com>
2024-02-22 10:20:59 +0900

    대시보드 수정 12

--- src/components/Dashboard/UserBar.jsx
+++ src/components/Dashboard/UserBar.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
-import Chart from "react-apexcharts";
+import React, { useEffect, useState } from "react";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 const BarChart = ({ data, user }) => {
-  const [userNames, setUserNames] = useState([]);
-  const [chartSeries, setChartSeries] = useState([]);
+  const [chartOptions, setChartOptions] = useState({});
 
   useEffect(() => {
     if (!data || !user) {
@@ -13,15 +13,14 @@ const BarChart = ({ data, user }) => {
 
     const idSet = new Set(data.map(item => item.t_ID));
     const ids = [...idSet];
-    console.log("IDs:", ids);
 
-    const datas = ids.map(id => {
+    const seriesData = ids.map(id => {
       const userData = user.find(userItem => userItem.b_ID === id);
       if (!userData) {
         console.log(`ID ${id}에 해당하는 사용자 데이터를 찾을 수 없습니다.`);
         return 0;
       }
-      console.log(data)
+
       const relevantData = data.filter(dataItem => dataItem.t_ID === id);
       if (relevantData.length === 0) {
         console.log(`ID ${id}에 해당하는 데이터가 없습니다.`);
@@ -33,57 +32,52 @@ const BarChart = ({ data, user }) => {
       return averageAccuracy;
     });
 
-    setUserNames(ids.map(id => user.find(userItem => userItem.b_ID === id).b_NAME));
-    setChartSeries([{ data: datas }]);
+    // 각 사용자에 대해 새로운 시리즈를 생성하고 랜덤한 색상 할당
+    const series = ids.map((id, index) => ({
+      name: user.find(userItem => userItem.b_ID === id).b_NAME,
+      data: [seriesData[index]],
+      color: `#${Math.floor(Math.random()*16777215).toString(16)}`
+    }));
+
+    setChartOptions({
+      chart: {
+        type: 'bar',
+        height: 300,
+        width: 300
+      },
+      title: {
+        text: ''
+      },
+      xAxis: {
+        visible: false
+      },
+      yAxis: {
+        title: {
+          text: null
+        },
+        min: 0,
+        max: 100,
+        tickAmount: 5
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true,
+            formatter: function () {
+              return this.y + '%';
+            }
+          },
+          tooltip: {
+            headerFormat: '',
+            pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}%</b><br/>'
+          }
+        }
+      },
+      series: series
+    });
   }, [data, user]);
 
-  const chartOptions = {
-    chart: {
-      id: "bar-chart",
-      toolbar: { show: false },
-      background: "transparent",
-      height: 300,
-      width: 500
-    },
-    xaxis: {
-      categories: userNames,
-      labels: { 
-        show: true,
-        offsetY: -8,
-        style: {
-          fontSize: '8px'
-        }
-      }
-    },
-    yaxis: {
-      labels: {
-        formatter: (value) => `${value}%`
-      },
-      min: 0,
-      max: 100,
-      tickAmount: 5
-    },
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "50%",
-        endingShape: "rounded"
-      }
-    },
-    colors: user ? user.map(() => `#${Math.floor(Math.random()*16777215).toString(16)}`) : [],
-    tooltip: {
-      y: { formatter: (value) => `${value}%` }
-    }
-  };
-
-  return (
-    <Chart
-      options={chartOptions}
-      series={chartSeries}
-      type="bar"
-      width={300}
-    />
-  );
+  return <HighchartsReact highcharts={Highcharts} options={chartOptions} />;
 };
 
 export default BarChart;
